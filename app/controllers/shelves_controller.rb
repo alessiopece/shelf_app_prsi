866b023ac0fd294ab6c8d8a5222edaa1d11a9444
Alessio Pece <skep.mail@gmail.com>
2015-07-02 18:01:08 +0200

    Creazione mensole incompleta (fino a listato 11.38)

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,2 +1,20 @@
 class ShelvesController < ApplicationController
+  def create
+    @shelf = current_user.shelves.build(shelf_params)
+    if @shelf.save
+      flash[:success] = "Shelf added!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+private
+
+    def shelf_params
+      params.require(:shelf).permit(:content)
+    end
 end
