83319d84d3915dc1e5eee31672a8d311e7336cd1
Alessio Pece <skep.mail@gmail.com>
2015-07-02 22:45:02 +0200

    Shelf creation/destruction

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,4 +1,6 @@
 class ShelvesController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
   def create
     @shelf = current_user.shelves.build(shelf_params)
     if @shelf.save
@@ -10,6 +12,10 @@ class ShelvesController < ApplicationController
   end
 
   def destroy
+    @shelf.destroy
+    flash[:success] = "Shelf deleted"
+    redirect_to request.referrer || root_url
+
   end
 
 private
@@ -17,4 +23,9 @@ private
     def shelf_params
       params.require(:shelf).permit(:content)
     end
+    def correct_user
+      @shelf = current_user.shelves.find_by(id: params[:id])
+      redirect_to root_url if @shelf.nil?
+    end
+
 end
