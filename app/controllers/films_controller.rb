df4034ac4cbd2d21d9b7e0a6082629e7587aa99f
Lorenzo Gallucci <lorygallu@gmail.com>
2015-07-05 13:11:29 +0200

    added film's controller

---
+++ app/controllers/films_controller.rb
@@ -0,0 +1,23 @@
+class FilmsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
+  def create
+    @film = Film.new
+    @film.shelf_id = params[:shelf_id] if params.has_key?(:shelf_id)
+    @film.title = params[:title] if params.has_key?(:title) 
+    if @film.save
+      flash[:success] = "Film added!"
+      redirect_to current_user
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+private
+
+
+end
